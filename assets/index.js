d58a6df43cf2952cec024657b5decd625a79db3a
Thyago Neumann <thyagotmn@gmail.com>
2022-04-05 09:41:34 -0300

    second

--- assets/index.js
+++ assets/index.js
@@ -1,5 +1,3 @@
-console.log('r/place')
-
 const canvas = document.getElementById('mainCanvas')
 
 canvas.width = window.innerWidth
@@ -7,46 +5,88 @@ canvas.height = window.innerHeight
 
 let context = canvas.getContext('2d')
 
-function Circle(x, y, dx, dy, radius, color) {
+let mouse = {
+    x: undefined,
+    y: undefined
+}
+window.addEventListener('resize', () => {
+    canvas.width = window.innerWidth
+    canvas.height = window.innerHeight
+})
+window.addEventListener('mousemove' , moveFunction, false)
+
+function moveFunction(event) {
+    mouse.x = event.x
+    mouse.y = event.y
+}
+const maxRadius = 50
+// const minRadius = 10
+
+function makeColor () {
+    const randomColor = Math.floor(Math.random()*16777215).toString(16);
+    const color = '#' + randomColor
+    return color
+}
+
+function Circle(x, y, dx, dy, radius) {    
     this.x = x
     this.y = y
     this.dx = dx
     this.dy = dy
     this.radius = radius
-    this.color = color
+    this.minRadius = radius
+    this.color = makeColor()
 
     this.draw = () => {
         context.beginPath()
         context.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false)
-        context.strokeStyle = color
+        context.strokeStyle = this.color
+        // context.fill()
         context.stroke()
-        context.fill()
     }
     this.update = () => {
         if (this.x + this.radius > innerWidth || this.x - this.radius < 0) {
             this.dx = -this.dx
-            this.color = 'bump'
+            this.bump = true
         }
         if (this.y + this.radius > innerHeight || this.y - this.radius < 0) {
             this.dy = -this.dy
-            this.color = 'bump'
+            this.bump = true
         }
         this.x += this.dx
         this.y += this.dy
+
+        // interactivity
+        if ( mouse.x - this.x < 50 
+            && mouse.x - this.x > -50 
+            && mouse.y - this.y < 50 
+            && mouse.y - this.y > -50 ) {
+            if ( this.radius < maxRadius ) {
+                this.radius += 1
+            }
+        } else if ( this.radius > this.minRadius ) {
+            this.radius -= 1
+        }
+        // bump
+        if ( this.bump ) {
+            context.strokeStyle = 'black'
+            this.draw()
+            this.bump = false
+        }
 
         this.draw()
     }
 }
 
 let circleArray = []
-let radius = 30
 
-for ( i = 0; i < 100; i++) {
+for ( i = 0; i < 500; i++) {
+    let radius = Math.random() * 10 + 1
     let x = Math.random() * (innerWidth - radius * 2) + radius
     let y = Math.random() * (innerHeight - radius *2) + radius
     let dx = (Math.random() - 0.5) *4
     let dy = (Math.random() - 0.5) *4
-    circleArray.push(new Circle(x, y, dx, dy, radius, 'black'))
+    circleArray.push(new Circle(x, y, dx, dy, radius))
 }
 
 function animate() {
@@ -54,21 +94,6 @@ function animate() {
     context.clearRect(0,0, innerWidth, innerHeight)
 
     for (i = 0; i < circleArray.length; i++){
-        let newRadius = (circleArray[i].radius + 1)
-        let x = Math.random() * (innerWidth - circleArray[i].radius * 2) + circleArray[i].radius
-        let y = Math.random() * (innerHeight - circleArray[i].radius * 2) + circleArray[i].radius
-        let dx = (Math.random() - 0.5) *4
-        let dy = (Math.random() - 0.5) *4
-        let randomColor = Math.floor(Math.random()*16777215).toString(16);
-        let color = '#' + randomColor
-        if (circleArray.length < 1000) {
-            if (circleArray[i].color === 'bump') {     
-                circleArray[i].color = color 
-                circleArray.push(new Circle(circleArray[i].x, circleArray[i].y, circleArray[i].dx / 2, circleArray[i].dy / 2, radius , color))
-                const index = circleArray.indexOf(circleArray[i])
-                circleArray.splice(index, 1)
-            }
-        }        
         circleArray[i].update()
     }
 }
